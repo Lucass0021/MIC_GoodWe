41675aafbaabc61c2586db023759bcf57c79b44a
LucasWerppFranco <146235988+LucasWerppFranco@users.noreply.github.com>
2025-09-30 19:46:52 -0300

    adicionando readme ao agente Cypher

--- badrock/Cypher/teste.py
+++ badrock/Cypher/teste.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-genai.configure(api_key=os.getenv("AIzaSyCJSLfW6d_3nABSQxRLaed6CDZR8_PmIt0"))
+genai.configure(api_key=os.getenv("AIzaSyDNBtOsHQ8S8sL3J_I8vinhNOH_g2qwuMg"))
 
 for m in genai.list_models():
     print(m.name, "->", m.supported_generation_methods)
