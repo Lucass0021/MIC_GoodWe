3bf4d827e5f7ea8d04f35c68615083e3bd70b892
Lucas Alves Antunes <llucas.alves0505@gmail.com>
2025-10-01 16:24:37 -0300

    Create __init__.py

---
+++ projeto_monitoramento/components/__init__.py
@@ -0,0 +1,13 @@
+from .header import render_header
+from .sidebar import render_sidebar
+from .kpi_cards import render_kpi_cards
+from .device_cards import render_device_cards
+from .charts import render_charts
+
+__all__ = [
+    'render_header',
+    'render_sidebar', 
+    'render_kpi_cards',
+    'render_device_cards',
+    'render_charts'
+]
